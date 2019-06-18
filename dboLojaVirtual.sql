17978ad4230d47a18bd41b9593f9510497ca9bdf
thicorey <31259154+thicorey@users.noreply.github.com>
2019-06-18 17:37:15 -0300

    Add files via upload

---
+++ dboLojaVirtual.sql
@@ -0,0 +1,16 @@
+create database lojavirutal
+
+create table Cadastro(
+id int (primary key), 
+nomeCampanha varchar(100))
+dataVigencia date 
+
+create table Usuario(
+idUsuario int(primary key),
+nome varchar(100),
+userId varchar(20),
+password varchar(20),
+email varchar(150),
+timeCoracao varchar(100),
+
+
